f0518c1f5a762dd105c904cd914162f1ba850090
walidKH007 <walid.khlouf@hotmail.com>
2020-01-20 22:03:25 +0100

    Delete create.py

--- create.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import pika
-connection = pika.BlockingConnection(pika.ConnectionParameters(host='192.168.56.101'))
-channel = connection.channel()
-channel.queue_declare(queue='M1RT-DAS')
-print("the queue create successfully")
-connection.close()
